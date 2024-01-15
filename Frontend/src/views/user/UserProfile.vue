4f4ae9bf3f004f7521cffb1e2c2d5c73607ebb31
namansehwal <namansehwal@email.com>
2024-01-16 05:27:02 +0530

    new updates

--- Frontend/src/views/user/UserProfile.vue
+++
@@ -1,6 +0,0 @@
-<script >
-</script>
-
-<template>
-    <h1>Welcome to your user profile!</h1>
-</template>
--- Frontend/src/views/user/UserSpace.vue
+++
@@ -1,6 +0,0 @@
-<script >
-</script>
-
-<template>
-    <h2>Welcome to your user space!</h2>
-</template>
